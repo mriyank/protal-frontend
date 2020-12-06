e18c69d957b2d8171f8c78776a235987e66c7b87
mriyank <mriyanksingh@gmail.com>
2020-12-06 17:21:47 +0530

    login page

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../App.css";
+
+// import { Route, Switch, Redirect } from "react-router-dom";
+ import LoginForm from './loginForm';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm className="LoginForm" />
+    </div>
+  );
+}
+
+export default App;
