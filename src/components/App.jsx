0b5a10c44285f9f516f48feaf6636502d7f12da2
mriyank <mriyanksingh@gmail.com>
2020-12-06 22:48:31 +0530

    login page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 import "../App.css";
+// import ReactDOM from "react-dom";
+import { BrowserRouter as Router,Link } from "react-router-dom";
+// import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
 // import { Route, Switch, Redirect } from "react-router-dom";
- import LoginForm from './loginForm';
-
-
+import LoginForm from "./loginForm";
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm className="LoginForm" />
-    </div>
+    <Router>
+      <div className="App">
+        {/* <LoginForm className="LoginForm" /> */}
+      </div>
+    </Router>
   );
 }
 
